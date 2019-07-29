5f75c0e95673c0f01142be1052b98e39391274e6
Jakub Gucwa <jakub.gucwa@outlook.com>
2019-07-29 23:55:02 +0200

    100729_API-koniec security autoryzacja tokenow

--- DatingApp.API/DatingApp.API/Controllers/AuthController.cs
+++ DatingApp.API/DatingApp.API/Controllers/AuthController.cs
@@ -8,12 +8,14 @@ using System.Threading.Tasks;
 using DatingApp.API.Data;
 using DatingApp.API.Dtos;
 using DatingApp.API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.API.Controllers
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class AuthController :ControllerBase
